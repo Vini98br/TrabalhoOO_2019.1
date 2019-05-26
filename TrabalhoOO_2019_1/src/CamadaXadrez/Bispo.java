271baaa71c027b1d29b160fb40bf6cfd96dd2aeb
Vini98br <48522066+Vini98br@users.noreply.github.com>
2019-05-26 11:06:21 -0300

    Add files via upload
    estrutura montada

---
+++ TrabalhoOO_2019_1/src/CamadaXadrez/Bispo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CamadaXadrez;
+
+/**
+ *
+ * @author vinic
+ */
+public class Bispo extends PecaXadrez
+{
+    
+}
