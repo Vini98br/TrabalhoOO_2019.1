e79d1746121555d77040a961348729dec7fa210d
Vini98br <viniciuss10@hotmail.com>
2019-06-23 14:17:26 -0300

    Montando classe Tabuleiro Exception

--- TrabalhoOO_2019_1/src/CamadaTabuleiro/Exception/TabuleiroException.java
+++ TrabalhoOO_2019_1/src/CamadaTabuleiro/Exception/TabuleiroException.java
@@ -11,5 +11,8 @@ package CamadaTabuleiro.Exception;
  */
 public class TabuleiroException extends RuntimeException
 {
-    
+    public TabuleiroException(String msg)
+    {
+        super(msg);
+    }
 }
