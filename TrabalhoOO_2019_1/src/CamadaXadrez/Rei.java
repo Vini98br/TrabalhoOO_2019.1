60f37b49b5fec56541bd7d6557dbc41ad1e89145
pedrocotta98 <37852363+pedrocotta98@users.noreply.github.com>
2019-06-29 16:39:05 -0300

    add coisas no rei

--- TrabalhoOO_2019_1/src/CamadaXadrez/Rei.java
+++ TrabalhoOO_2019_1/src/CamadaXadrez/Rei.java
@@ -14,12 +14,82 @@ public class Rei extends PecaXadrez implements Movimento{
     public String toString(){
         return "k";
     }
+    private boolean podeMover(Posicao posicao) {
+        PecaXadrez p = (PecaXadrez) getTabuleiro().peca(posicao);
+        return p == null || p.getCor() != getCor();
+    }
+
     @Override
-    public boolean[][] movimentosPossiveis()
-    {
+    public boolean[][] movimentosPossiveis() {
         boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+        Posicao p=new Posicao(0,0);
+        
+        //acima
+        p.setLinha(posicao.getLinha()-1);
+        p.setLinha(posicao.getColuna());
+        if(getTabuleiro().posicaoExiste(p)&& podeMover(p))
+        {
+            mat[p.getLinha()][p.getColuna()]=true;
+        }
+        
+        //abaixo
+        p.setLinha(posicao.getLinha()+1);
+        p.setLinha(posicao.getColuna());
+        if(getTabuleiro().posicaoExiste(p)&& podeMover(p))
+        {
+            mat[p.getLinha()][p.getColuna()]=true;
+        }
+        
+        //esquerda
+        p.setLinha(posicao.getLinha());
+        p.setLinha(posicao.getColuna()-1);
+        if(getTabuleiro().posicaoExiste(p)&& podeMover(p))
+        {
+            mat[p.getLinha()][p.getColuna()]=true;
+        }
+        
+        //direita
+        p.setLinha(posicao.getLinha());
+        p.setLinha(posicao.getColuna()+1);
+        if(getTabuleiro().posicaoExiste(p)&& podeMover(p))
+        {
+            mat[p.getLinha()][p.getColuna()]=true;
+        }
+        //DIAGONAL NOROESTE
+        p.setLinha(posicao.getLinha()-1);
+        p.setLinha(posicao.getColuna()-1);
+        if(getTabuleiro().posicaoExiste(p)&& podeMover(p))
+        {
+            mat[p.getLinha()][p.getColuna()]=true;
+        }
+        //DIAGONAL NORDESTE
+        p.setLinha(posicao.getLinha()-1);
+        p.setLinha(posicao.getColuna()+1);
+        if(getTabuleiro().posicaoExiste(p)&& podeMover(p))
+        {
+            mat[p.getLinha()][p.getColuna()]=true;
+        }
+        //DIAGONAL SUDOESTE
+        p.setLinha(posicao.getLinha()+1);
+        p.setLinha(posicao.getColuna()-1);
+        if(getTabuleiro().posicaoExiste(p)&& podeMover(p))
+        {
+            mat[p.getLinha()][p.getColuna()]=true;
+        }
+        //DIAGONAL SUDESTE
+        p.setLinha(posicao.getLinha()+1);
+        p.setLinha(posicao.getColuna()+1);
+        if(getTabuleiro().posicaoExiste(p)&& podeMover(p))
+        {
+            mat[p.getLinha()][p.getColuna()]=true;
+        }
+        
+        
+        
+        
         return mat;
     }
+
 
    @Override
     public boolean possivelMovimento(Posicao posicao)
