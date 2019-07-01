616c882b032641ecb2feb2df819e8fef88e6473a
LaraPompei <pompei.lara@engenharia.ufjf.br>
2019-06-30 23:29:39 -0300

    rainha movimentos

--- TrabalhoOO_2019_1/src/CamadaXadrez/Rainha.java
+++ TrabalhoOO_2019_1/src/CamadaXadrez/Rainha.java
@@ -6,25 +6,110 @@ import CamadaXadrez.Enum.Cor;
 
 public class Rainha extends PecaXadrez implements Movimento{
 
-    
-     
     public Rainha(Tabuleiro tabuleiro, Cor cor){
         super(tabuleiro, cor);
     }
     
     @Override
-    public String toString()
-    {
+    public String toString(){
         return "Q";
     }
     
-    @Override
+  @Override
     public boolean[][] movimentosPossiveis()
     {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean[][] mat = movimentosPossiveis();
+        Posicao p = new Posicao(0, 0);
+        
+        //acima
+        p.setValor(posicao.getLinha() - 1, posicao.getColuna());
+        while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setLinha(posicao.getLinha() - 1);
+        }
+        if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        //esquerda
+        p.setValor(posicao.getLinha(), posicao.getColuna() - 1);
+        while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setColuna(posicao.getColuna() - 1);
+        }
+        if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        //esquerda
+        p.setValor(posicao.getLinha(), posicao.getColuna() + 1);
+        while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setColuna(posicao.getColuna() + 1);
+        }
+        if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        //abaixo
+        p.setValor(posicao.getLinha() + 1, posicao.getColuna());
+        while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setLinha(posicao.getLinha() + 1);
+        }
+        if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        //NOROESTE
+        p.setValor(posicao.getLinha() - 1, posicao.getColuna()-1);
+        while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setLinha(posicao.getLinha() - 1);
+            p.setColuna(posicao.getColuna() - 1);
+        }
+        if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        //NORDESTE
+        p.setValor(posicao.getLinha()-1, posicao.getColuna() + 1);
+        while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setColuna(posicao.getColuna() + 1);
+            p.setLinha(posicao.getLinha() - 1);
+        }
+        if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        //SUDESTE
+        p.setValor(posicao.getLinha()+1, posicao.getColuna() + 1);
+        while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setColuna(posicao.getColuna() + 1);
+            p.setLinha(posicao.getLinha() + 1);
+        }
+        if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        //SUDOESTE
+        p.setValor(posicao.getLinha() + 1, posicao.getColuna()-1);
+        while(getTabuleiro().posicaoExiste(p) && !getTabuleiro().temPeca(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setLinha(posicao.getLinha() + 1);
+            p.setColuna(posicao.getColuna() + -1);
+        }
+        if(getTabuleiro().posicaoExiste(p) && temPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        return mat;
     }
+    
 
-      @Override
+  @Override
     public boolean possivelMovimento(Posicao posicao)
     {
        return movimentosPossiveis()[posicao.getLinha()][posicao.getColuna()]; 
@@ -33,7 +118,7 @@ public class Rainha extends PecaXadrez implements Movimento{
     @Override
     public boolean temAlgumPossivelMovimento()//Roda a matriz em busca de ao menos um movimento possivel na matriz
     {
-        boolean[][] mat = movimentosPossiveis();
+         boolean[][] mat = movimentosPossiveis();
         for(int i=0 ; i<mat.length; i++)
         {
             for(int j=0; j<mat.length;j++)
@@ -45,6 +130,6 @@ public class Rainha extends PecaXadrez implements Movimento{
             }
         }
         return false;
-    }
     
+    }
 }
